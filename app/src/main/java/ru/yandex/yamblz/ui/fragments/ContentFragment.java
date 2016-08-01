965b360d5687974796a6db1d16ff97abcb43dd9b
Veda Voronina <veda345@yandex.ru>
2016-08-02 02:08:51 +0300

    first animation done

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -1,6 +1,8 @@
 package ru.yandex.yamblz.ui.fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
